5d03005e59a75e1f87d0f289c0098ff854dfa236
Hoai CV <nguyenhoaiqt32@gmail.com>
2025-05-16 16:38:15 +0700

    [AT]: add file practice_5

---
+++ tests/practice_5/practice_5.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test';
+
+test.skip('Open new window and navigate back', async ({context, page}) => {
+    await page.goto('http://127.0.0.1:3000/');
+    const pageProsime = context.waitForEvent('page');
+    await page.click('#openNewWindow');
+    const newPage = await pageProsime;
+    await newPage.waitForLoadState();
+    console.log(await newPage.title());
+    await expect(newPage.getByRole('heading', {name: 'Welcome to the New Page' })).toBeVisible();
+});
+
+test.skip('Add Cookie', async ({page}) => {
+    await page.goto('http://127.0.0.1:3000/');
+    await page.click('#setCookie');
+
+    const cookies = await page.context().cookies();
+    const sessionCookie = cookies.find(c => c.name === 'session');
+    console.log('Session cookie', sessionCookie);
+
+    await expect(sessionCookie).toBeDefined();
+    
+});
+
